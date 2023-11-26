16aa6a162acee99e8d108c4483505304d0ed285a
Paul Buser <buser.paul@gmail.com>
2023-11-25 20:16:23 -0500

    Flesh out server implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
-// 1. Select the div element using the id property
-const app = document.getElementById("app");
-// 2. Create a new <p></p> element programmatically
-const p = document.createElement("p");
-// 3. Add the text content
-p.textContent = "Hello, World!";
-// 4. Append the p element to the div element
-app?.appendChild(p);
+import { io } from "socket.io-client";
+
+const sio = io("http://127.0.0.1:3000/");
+
+sio.on("monotonic_time", (time) => {
+    _time = parseBigIntStringToBigInt(time);
+})
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+var _time: bigint = BigInt(0);
+
+const width = canvas.width;
+const height = canvas.height;
+
+let ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+setInterval(() => {
+    // Clear the canvas
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(width / 2, height / 2 + cyclicSine(_time) * 50, 25, 25);
+}, 16);
+
+function parseBigIntStringToBigInt(bigIntString: string): bigint {
+    return BigInt(bigIntString);
+}
+
+function nanosecondsToSeconds(nanoseconds: bigint): number {
+    return Number(nanoseconds) / 1e9;
+}
+
+function cyclicSine(timestamp: bigint): number {
+    const seconds = nanosecondsToSeconds(timestamp);
+    const frequency = 0.3; // Adjust the frequency as needed
+    const amplitude = 1.0; // Adjust the amplitude as needed
+    return amplitude * Math.sin(2 * Math.PI * frequency * seconds);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import { Server } from "socket.io";
 
-const io = new Server({ /* options */ });
-
-io.on("connection", (socket) => {
-    console.log("someone connected!");
+const io = new Server({
+    cors: {
+        origin: "http://localhost:1234",
+        methods: ["GET", "POST"]
+    }
 });
+
+// Emit time at roughly 60Hz
+setInterval(() => {
+    io.emit("monotonic_time", process.hrtime.bigint().toString());
+}, 16);
 
 console.log("running on port 3000...");
 io.listen(3000);
